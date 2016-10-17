e3de37dc0e6ae05a338c17c3d5bb53f7168bbb34
8R2BJA8B78MBXVG\Administrator <cmf1903980333@qq.com>
2016-10-17 15:36:04 +0800

    update

---
+++ src/js/components/index.js
@@ -0,0 +1,7 @@
+export { default as Nav } from './Nav'
+export { default as Header } from './Header'
+export { default as FastNav } from './FastNav'
+export { default as Title } from './Title'
+export { default as SelectMenu } from './SelectMenu'
+export { default as ShopList } from './ShopList'
+export { default as Message } from './Message'
